4bfaa8ad2d7475cd221d6d3148f3d4cdafc8e177
MoSaif00 <69274977+MoSaif00@users.noreply.github.com>
2021-07-18 00:47:29 +0200

    counting letters

--- ExUnit5.py
+++ ExUnit5.py
@@ -1,8 +1,14 @@
-prefixes = 'JKLMNOPQ'
-suffix = 'ack'
-for letter in prefixes:
-    if letter == "O" or letter == "Q":
-        print(letter + "u" + suffix)
-    else:
-        print(letter + suffix)
+# prefixes = 'JKLMNOPQ'
+# suffix = 'ack'
+# for letter in prefixes:
+#     if letter == "O" or letter == "Q":
+#         print(letter + "u" + suffix)
+#     else:
+#         print(letter + suffix)
 
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
