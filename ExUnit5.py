ce8f417d4910b9dc8dac4987679a86ebfe90834a
MoSaif00 <69274977+MoSaif00@users.noreply.github.com>
2021-07-18 01:56:17 +0200

    practice

--- ExUnit5.py
+++ ExUnit5.py
@@ -6,9 +6,28 @@
 #     else:
 #         print(letter + suffix)
 
-word = 'banana'
-count = 0
-for letter in word:
-    if letter == 'a':
-        count = count + 1
-print(count)
+# word = 'banana'
+# count = 0
+# for letter in word:
+#     if letter == 'a':
+#         count = count + 1
+# print(count)
+
+# while True:
+
+#     while 1 > 0:
+
+#         break
+
+#     print("Got it!")
+
+#     break
+
+# n = 10
+# while n != 1:
+#     print (n,)
+#     if n % 2 == 0: # n is even
+#         n = n // 2
+#     else: # n is odd
+#         n = n * 3 + 1
+
