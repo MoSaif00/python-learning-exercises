7e047a4c7ac74c83c95938c7fc2fd2bd9b9d59bc
MoSaif00 <69274977+MoSaif00@users.noreply.github.com>
2021-07-24 23:18:09 +0200

    lists

---
+++ dfun6.py
@@ -0,0 +1,29 @@
+# object1 = 'Saif'
+# object2 = 'Saif'
+
+# print("object1 and object2 are equivalent ",object2 == object1)
+# print("object1 and object2 are identical ",object2 is object1)
+
+# object3 = ['Mo','Sa','Abdu']
+# object4 = ['Mo','Sa','Abdu']
+
+# print("object3 and object4 are equivalent ",object4 == object3)  #check equivalent of value 
+# print("object3 and object4 are identical ",object4 is object3)  # check identical
+
+# object5 = ['Mo','Sa','Abdu']
+# object6 = object5
+
+# print("object5 and object6 are equivalent ",object6 == object5)
+# print("object5 and object6 are identical ",object6 is object5)
+
+
+# RefObject = ["h","e","l","l","o"]
+# secondRefObject = RefObject
+# thirdObject = secondRefObject
+# print(thirdObject)
+
+# thirdObject[1]="a"
+# print(RefObject)
+# print(secondRefObject)
+# print(thirdObject)
+
